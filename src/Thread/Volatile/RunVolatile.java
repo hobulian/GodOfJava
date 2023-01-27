85a62542c4892a506afa9b64735af0dd6d65e54b
amandel <hobulian@gmail.com>
2023-01-27 21:15:56 +0900

    Volatile 관련 주석 추가

--- src/Thread/Volatile/RunVolatile.java
+++ src/Thread/Volatile/RunVolatile.java
@@ -1,5 +1,12 @@
 package Thread.Volatile;
 
+/* Volatile
+    변수를 CPU 캐시가 아닌 Main Memory에 저장하겠다는 선언
+    변수를 Read Write할 때마다 주 메모리에 접근
+    하나의 Thread가 Write하고 나머지 Thread가 읽는 상황에 변수 값이 일치하지 않게 될 수 있음
+    혹은 변수의 값이 최신의 값으로 읽혀야 하는 경우
+    https://nesoy.github.io/articles/2018-06/Java-volatile
+ */
 public class RunVolatile {
     public static void main(String[] args) {
         RunVolatile sample = new RunVolatile();
